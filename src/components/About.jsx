16d3259b6d0d3607194df961a9945ba4767d8c5b
jaikrishnaverma-dev <jaikrishnaverma@gmail.com>
2024-05-05 17:21:53 +0530

    card dynamicity done

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = ({ job }) => {
+    console.log({job});
+  return (
+    <div className="w-100 about">
+      <h5 className="about_heading">About Company</h5>
+      <p className="about_description">{job?.jobDetailsFromCompany}</p>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,74 @@
 import React, { useEffect } from "react";
 import Navbar from "./Navbar";
-import { Container, Grid } from "@mui/material";
+import { Box, Button, Container, Modal } from "@mui/material";
 import JobCards from "./JobCards";
 import { useSelector } from "react-redux";
-import mainSlice from "../redux/mainSlice";
+import About from "./About";
+import RefreshIcon from "@mui/icons-material/Refresh";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  maxWidth: 650,
+  height: 500,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  overflowY: "scroll",
+  borderRadius: 5,
+  p: 4,
+};
 const Home = () => {
-  const { jobs, loading } = useSelector((data) => {
-    console.log({ data });
-    return data.mainSlice;
+  const [modal, setModal] = React.useState({
+    open: false,
+    job: {},
   });
-  useEffect(() => {
-    console.log({ jobs });
-  }, [jobs]);
+
+  const handleOpen = (job) => {
+    setModal({ open: true, job: job });
+  };
+  const handleClose = () => setModal({ open: false, job: {} });
+
+  const { jobs, loading } = useSelector((data) => data.mainSlice);
 
   return (
-    <Container maxWidth="xl">
+    <Container maxWidth="lg">
       <Navbar />
-      <div style={{marginTop:"30px"}}>
-        {loading ? (
-          <>Loading...</>
-        ) : (
-          <Grid container spacing={2}>
-            {jobs.map((el) => (
-              <Grid xs={12} md={4}>
-                <JobCards />
-              </Grid>
-            ))}
-          </Grid>
-        )}
+      <div>
+        <div className="cards-wrapper">
+          {jobs.map((job) => (
+            <JobCards key={job?.jdUid} job={job} handleShowMore={handleOpen} />
+          ))}
+        </div>
+        <div
+          className="w-100"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            padding: "20px",
+            marginBottom: "10px",
+          }}
+        >
+          <Button
+            variant="text"
+            loading={true}
+            startIcon={loading ? "" : <RefreshIcon />}
+          >
+            {loading ? "Loading..." : "Load more"}
+          </Button>
+        </div>
       </div>
+
+      <Modal
+        open={modal.open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          {Object.keys(modal.job).length > 0 ? <About job={modal.job} /> : "NA"}
+        </Box>
+      </Modal>
     </Container>
   );
 };
